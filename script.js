e9083e46052ea2ab7cd197186234d9060f1e575a
mariaeugeniacecchini <cecchinitest@gmail.com>
2025-05-26 13:07:40 -0300

    JS Login, cambio de botones

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("mensaje de consola");
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById('hamburger');
+  const navMenu = document.getElementById('nav-menu');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+
+    if (navMenu.classList.contains('show')) {
+      hamburger.textContent = '✖'; // Icono cerrar
+    } else {
+      hamburger.textContent = '☰'; // Icono hamburguesa
+    }
+  });
+});
